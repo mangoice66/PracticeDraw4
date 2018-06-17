f2f5ff8d9cd373c1c0e5ea4d1d616d04066e32dc
MangoIce <raodunwei@163.com>
2018-06-17 21:07:56 +0800

    Initial commit

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -0,0 +1,67 @@
+package com.hencoder.hencoderpracticedraw4.practice;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.hencoderpracticedraw4.R;
+
+public class Practice12CameraRotateFixedView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+    Point point1 = new Point(200, 200);
+    Point point2 = new Point(600, 200);
+    Camera camera = new Camera();
+
+    public Practice12CameraRotateFixedView(Context context) {
+        super(context);
+    }
+
+    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        canvas.save();
+        camera.save();
+        camera.rotateX(30);
+        canvas.translate(point1.x + width / 2, point1.y + height / 2);
+        camera.applyToCanvas(canvas);
+        canvas.translate(- (point1.x + width / 2), - (point1.y + height / 2));
+        camera.restore();
+        canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+
+        canvas.save();
+        camera.save();
+        camera.rotateY(30);
+        canvas.translate(point2.x + width / 2, point2.y + height / 2);
+        camera.applyToCanvas(canvas);
+        canvas.translate(- (point2.x + width / 2), - (point2.y + height / 2));
+        camera.restore();
+        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
+    }
+}
